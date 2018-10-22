b88a69048e38477a906cc96687592f67aebbb782
lugges@tp420 <29121982+Lugges991@users.noreply.github.com>
2018-10-22 20:31:57 +0200

    still needs fixing

--- MCMC/MonteCarloIntegration.py
+++ MCMC/MonteCarloIntegration.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 def f(x):
-    y = 1 / np.sqrt(2 * np.pi) * np.exp(-(1 / 2) * np.square(x))
+    # y = 1 / np.sqrt(2 * np.pi) * np.exp(-(1 / 2) * np.square(x))
+    y = np.sqrt(14 * np.square(x)) / 32**x
     return y
 
 
@@ -23,29 +24,33 @@ def MC_integrate(lb, ub, n):
 
     # generate a random 2xn matrix P, drawn from the 'continuous uniform'
     # distribution over the stated interval
-    P = np.zeros((2, n))
+    x_rand = []
+    y_rand = []
     y_min = f(lb)
     y_max = y_min
     for i in range(n):
-        (P[0, i], P[1, i]) = (ub - lb) * np.random.random_sample((2, 1)) + lb
+        x_rand.append((ub - lb) * np.random.random_sample() + lb)
+        y_rand.append((ub - lb) * np.random.random_sample() + lb)
         # now check whether the Point P(x,y) lies under the graph of f(x)
         # therefore y < f(x)
-        if P[1, i] < f(P[0, i]):
-            hits.append((P[0, i], P[1, i]))
+        if y_rand[i] < f(x_rand[i]):
+            hits.append((x_rand, y_rand))
         else:
-            misses.append((P[0, i], P[1, i]))
+            misses.append((x_rand, y_rand))
 
         # determine y_min and y_max
-        if f(P[0, i]) < y_min:
-            y_min = f(P[0, i])
-        if f(P[0, i]) > y_max:
-            y_max = f(P[0, i])
+        if f(x_rand[i]) < y_min:
+            y_min = f(x_rand[i])
+        if f(x_rand[i]) > y_max:
+            y_max = f(x_rand[i])
     print(len(hits))
+    print(y_max, y_min)
     ratio = len(hits) / n
-    interval_area = (ub - lb) * (y_max - y_min)
+    interval_area = (ub - lb) * (2)
+    print(interval_area)
     Area = ratio * interval_area
     return Area
 
 
 if __name__ == '__main__':
-    pass
+    print(MC_integrate(0, 1, 1000))
